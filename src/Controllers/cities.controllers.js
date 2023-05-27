292902e3e11d32ebca8d28b8bb92e790cb8f5b36
Tiago <tiagoalex0202@gmail.com>
2023-05-27 07:10:22 -0300

    .

--- src/Controllers/cities.controllers.js
+++ src/Controllers/cities.controllers.js
@@ -7,7 +7,7 @@ export async function getCities (req, res ){
         for (let i=0; i < cities.rows.length; i++){
             names.push(cities.rows[i].name)
         }
-        return res.status(201).send(names)
+        return res.status(201).send(cities.rows)
     } catch(err){
         console.log(err.message)
     }
