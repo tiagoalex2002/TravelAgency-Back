40e3fded4d666bb56f53f9ccf7ef1c2fb2009c0f
Tiago <tiagoalex0202@gmail.com>
2023-05-28 15:11:07 -0300

    route fixing

--- src/Routes/hotels.routes.js
+++ src/Routes/hotels.routes.js
@@ -3,7 +3,7 @@ import { getHotels,getHotelDetail } from "../Controllers/hotels.controllers.js";
 
 const hotelsRouter= Router()
 
-hotelsRouter.get("/hotels/:city",getHotels)
+hotelsRouter.get("/hotels/:cityId",getHotels)
 hotelsRouter.get("/hotels/:id", getHotelDetail)
 
 export default hotelsRouter;
